7e6a22935ec8122d0fd4963b1c83db5f042aa506
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-07-01 12:32:03 -0300

    remove "p_date" and "p_id" cols, only leave "Page" and "Link"

--- scripts/posts_from_xlsx.py
+++ scripts/posts_from_xlsx.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import openpyxl
-from datetime import datetime
 
 # Archivo de fanpage con las publicaciones del mes
 archivo  = r"./excel/Junio 2020.xlsx"
@@ -16,26 +15,13 @@ for row in ws.iter_rows(min_row=10, min_col=12, max_col=12):
             pass
 wb.save(archivo)
 
-# Abrir archivo en pandas, y agarrar las cosas que importan
+# Abrir archivo en pandas, y seleccionar los posteos de instagram de cada usuario
 posteos = pd.read_excel(archivo, skiprows=10)
-posteos = posteos[["Date", "Network", "Page", "Link"]]
 posteos = posteos.loc[posteos['Network'] == "INSTAGRAM"]
 posteos = posteos.drop("Network", axis=1)
+posteos = posteos[["Page", "Link"]]
 
-# Nombre del banco -> usuario de ig
-posteos = posteos.replace("Banco Provincia", "banco_provincia")
-posteos = posteos.replace("Banco Ciudad", "banco.ciudad")
-posteos = posteos.replace("Banco Nación", "banconacion")
+# Reemplazar nombres de las páginas por sus usuarios de ig
+# posteos["Page"] = ["googlemaps" if "Google Maps" in p else p for p in posteos["Page"]]
 
-# Transformar date en timestamp y agregar columna de id
-posteos["Date"] = posteos["Date"].apply(lambda x: datetime.timestamp(x))
-posteos["p_id"] = posteos["Link"].apply(lambda x: x[-12:-1])
 
-# Renombrar y reordenar columnas
-posteos = posteos.rename(columns={"Page": "p_author", "Link": "p_link", "Date": "p_date"})
-posteos = posteos[["p_id", "p_author", "p_date", "p_link"]]
-
-master_path = r"./dictionaries/posts/master.csv"
-master = pd.read_csv(master_path)
-concat = pd.concat([master, posteos])
-concat.to_csv(master_path, index=False)
